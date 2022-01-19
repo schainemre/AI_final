bed1193b4665df357368b5d463a4fea82b02405d
emresahin2001 <67149817+emresahin2001@users.noreply.github.com>
2022-01-19 16:22:14 +0300

    odev

---
+++ Odev/Controllers/PredictController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Odev.MLModel;
+
+namespace Odev.Controllers
+{
+    public class PredictController : Controller
+    {
+        [HttpGet]
+        public IActionResult Predict()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Predict(ModelInput input)
+        {
+            var prediction = MLModel.Predict(input);
+            ViewBag.Result = prediction;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Predict1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Predict1(ModelInput input)
+        {
+            var prediction = MLModel.Predict(input);
+            ViewBag.Result = prediction;
+            return View();
+        }
+    }
+}
